010d98f77a656b8b2b3ba0c8db1d4bc2e8a4fae4
Jake Nulty <nultinator@yahoo.com>
2024-11-25 13:30:32 -0500

    Add files via upload

---
+++ crawler-pagination.py
@@ -0,0 +1,107 @@
+import os
+import csv
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import logging
+from urllib.parse import urlencode
+import concurrent.futures
+from dataclasses import dataclass, field, fields, asdict
+
+API_KEY = ""
+
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
+    API_KEY = config["api_key"]
+
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+
+
+## Logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+
+def scrape_search_results(keyword, location, locality, page_number, retries=3):
+    formatted_keyword = keyword.replace(" ", "+")
+    formatted_locality = locality.replace(" ", "+")
+    url = f"https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords={formatted_keyword}&location={formatted_locality}&original_referer=&start={page_number*10}"
+    tries = 0
+    success = False
+
+    
+    while tries <= retries and not success:
+
+        driver = webdriver.Chrome(options=options)
+
+        try:
+            driver.get(url)
+                        
+            div_cards = driver.find_elements(By.CSS_SELECTOR, "div[class='base-search-card__info']")
+
+            if not div_cards:
+                driver.save_screenshot("debug.png")
+                raise Exception("Page did not load correctly, please check debug.png")
+
+            for div_card in div_cards:
+                company_name = div_card.find_element(By.CSS_SELECTOR, "h4[class='base-search-card__subtitle']").text
+                print("company name", company_name)
+                job_title = div_card.find_element(By.CSS_SELECTOR, "h3[class='base-search-card__title']").text
+                parent = div_card.find_element(By.XPATH, "..")
+                link = parent.find_element(By.CSS_SELECTOR, "a")
+                job_link = link.get_attribute("href")
+                location = div_card.find_element(By.CSS_SELECTOR, "span[class='job-search-card__location']").text
+                
+                search_data = {
+                    "name": company_name,
+                    "job_title": job_title,
+                    "url": job_link,
+                    "location": location
+                }          
+
+                print(search_data)
+            logger.info(f"Successfully parsed data from: {url}")
+            success = True
+        
+                    
+        except Exception as e:
+            logger.error(f"An error occurred while processing page {url}: {e}")
+            logger.info(f"Retrying request for page: {url}, retries left {retries-tries}")
+            tries+=1
+
+        finally:
+            driver.quit()
+
+    if not success:
+        raise Exception(f"Max Retries exceeded: {retries}")
+
+
+
+
+def start_scrape(keyword, pages, location, locality, retries=3):
+    for page in pages:
+        scrape_search_results(keyword, location, locality, page, retries=retries)
+
+
+if __name__ == "__main__":
+
+    MAX_RETRIES = 3
+    MAX_THREADS = 5
+    PAGES = 1
+    LOCATION = "us"
+    LOCALITY = "United States"
+
+    logger.info(f"Crawl starting...")
+
+    ## INPUT ---> List of keywords to scrape
+    keyword_list = ["software engineer"]
+    aggregate_files = []
+
+    ## Job Processes
+    for keyword in keyword_list:
+        filename = keyword.replace(" ", "-")
+
+        start_scrape(keyword, PAGES, LOCATION, LOCALITY, retries=MAX_RETRIES)
+    logger.info(f"Crawl complete.")
---
+++ scraper-storage.py
@@ -0,0 +1,284 @@
+import os
+import csv
+import json
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import logging
+from urllib.parse import urlencode
+import concurrent.futures
+from dataclasses import dataclass, field, fields, asdict
+
+API_KEY = ""
+
+with open("config.json", "r") as config_file:
+    config = json.load(config_file)
+    API_KEY = config["api_key"]
+
+options = webdriver.ChromeOptions()
+options.add_argument("--headless")
+
+
+
+def get_scrapeops_url(url, location="us"):
+    payload = {
+        "api_key": API_KEY,
+        "url": url,
+        "country": location,
+        }
+    proxy_url = "https://proxy.scrapeops.io/v1/?" + urlencode(payload)
+    return proxy_url
+
+
+## Logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+
+@dataclass
+class SearchData:
+    name: str = ""
+    job_title: str = ""
+    url: str = ""
+    location: str = ""
+
+    def __post_init__(self):
+        self.check_string_fields()
+        
+    def check_string_fields(self):
+        for field in fields(self):
+            # Check string fields
+            if isinstance(getattr(self, field.name), str):
+                # If empty set default text
+                if getattr(self, field.name) == "":
+                    setattr(self, field.name, f"No {field.name}")
+                    continue
+                # Strip any trailing spaces, etc.
+                value = getattr(self, field.name)
+                setattr(self, field.name, value.strip())
+
+@dataclass
+class JobData:
+    name: str = ""
+    seniority: str = ""
+    position_type: str = ""
+    job_function: str = ""
+    industry: str = ""
+
+
+    def __post_init__(self):
+        self.check_string_fields()
+        
+    def check_string_fields(self):
+        for field in fields(self):
+            # Check string fields
+            if isinstance(getattr(self, field.name), str):
+                # If empty set default text
+                if getattr(self, field.name) == "":
+                    setattr(self, field.name, f"No {field.name}")
+                    continue
+                # Strip any trailing spaces, etc.
+                value = getattr(self, field.name)
+                setattr(self, field.name, value.strip())
+
+
+class DataPipeline:
+    
+    def __init__(self, csv_filename="", storage_queue_limit=50):
+        self.names_seen = []
+        self.storage_queue = []
+        self.storage_queue_limit = storage_queue_limit
+        self.csv_filename = csv_filename
+        self.csv_file_open = False
+    
+    def save_to_csv(self):
+        self.csv_file_open = True
+        data_to_save = []
+        data_to_save.extend(self.storage_queue)
+        self.storage_queue.clear()
+        if not data_to_save:
+            return
+
+        keys = [field.name for field in fields(data_to_save[0])]
+        file_exists = os.path.isfile(self.csv_filename) and os.path.getsize(self.csv_filename) > 0
+        with open(self.csv_filename, mode="a", newline="", encoding="utf-8") as output_file:
+            writer = csv.DictWriter(output_file, fieldnames=keys)
+
+            if not file_exists:
+                writer.writeheader()
+
+            for item in data_to_save:
+                writer.writerow(asdict(item))
+
+        self.csv_file_open = False
+                    
+    def is_duplicate(self, input_data):
+        if input_data.name in self.names_seen:
+            logger.warning(f"Duplicate item found: {input_data.name}. Item dropped.")
+            return True
+        self.names_seen.append(input_data.name)
+        return False
+            
+    def add_data(self, scraped_data):
+        if self.is_duplicate(scraped_data) == False:
+            self.storage_queue.append(scraped_data)
+            if len(self.storage_queue) >= self.storage_queue_limit and self.csv_file_open == False:
+                self.save_to_csv()
+                       
+    def close_pipeline(self):
+        if self.csv_file_open:
+            time.sleep(3)
+        if len(self.storage_queue) > 0:
+            self.save_to_csv()
+
+
+
+def scrape_search_results(keyword, location, locality, page_number, data_pipeline=None, retries=3):
+    formatted_keyword = keyword.replace(" ", "+")
+    formatted_locality = locality.replace(" ", "+")
+    url = f"https://www.linkedin.com/jobs-guest/jobs/api/seeMoreJobPostings/search?keywords={formatted_keyword}&location={formatted_locality}&original_referer=&start={page_number*10}"
+    tries = 0
+    success = False
+
+    
+    while tries <= retries and not success:
+
+        driver = webdriver.Chrome(options=options)
+
+        try:
+            scrapeops_proxy_url = get_scrapeops_url(url, location=location)
+            driver.get(scrapeops_proxy_url)
+                        
+            div_cards = driver.find_elements(By.CSS_SELECTOR, "div[class='base-search-card__info']")
+
+            if not div_cards:
+                driver.save_screenshot("debug.png")
+                raise Exception("Page did not load correctly, please check debug.png")
+
+            for div_card in div_cards:
+                company_name = div_card.find_element(By.CSS_SELECTOR, "h4[class='base-search-card__subtitle']").text
+                print("company name", company_name)
+                job_title = div_card.find_element(By.CSS_SELECTOR, "h3[class='base-search-card__title']").text
+                parent = div_card.find_element(By.XPATH, "..")
+                link = parent.find_element(By.CSS_SELECTOR, "a")
+                job_link = link.get_attribute("href")
+                location = div_card.find_element(By.CSS_SELECTOR, "span[class='job-search-card__location']").text
+                
+                search_data = SearchData(
+                    name=company_name,
+                    job_title=job_title,
+                    url=job_link,
+                    location=location
+                )           
+
+                data_pipeline.add_data(search_data)
+            logger.info(f"Successfully parsed data from: {url}")
+            success = True
+        
+                    
+        except Exception as e:
+            logger.error(f"An error occurred while processing page {url}: {e}")
+            logger.info(f"Retrying request for page: {url}, retries left {retries-tries}")
+            tries+=1
+
+        finally:
+            driver.quit()
+
+    if not success:
+        raise Exception(f"Max Retries exceeded: {retries}")
+
+
+
+
+def start_scrape(keyword, pages, location, locality, data_pipeline=None, max_threads=5, retries=3):
+    with concurrent.futures.ThreadPoolExecutor(max_workers=max_threads) as executor:
+        executor.map(
+            scrape_search_results,
+            [keyword] * pages,
+            [location] * pages,
+            [locality] * pages,
+            range(pages),
+            [data_pipeline] * pages,
+            [retries] * pages
+        )
+
+
+def process_posting(row, location, retries=3):
+    url = row["url"]
+    tries = 0
+    success = False
+
+    while tries <= retries and not success:
+        driver = webdriver.Chrome(options=options)
+        try:
+            driver.get(url, location=location)
+            
+            job_pipeline = DataPipeline(csv_filename=f"{row['name'].replace(' ', '-')}.csv")
+            
+            job_criteria = driver.find_elements(By.CSS_SELECTOR, "li[class='description__job-criteria-item']")
+            seniority = job_criteria[0].text.replace("Seniority level", "")
+            position_type = job_criteria[1].text.replace("Employment type", "")
+            job_function = job_criteria[2].text.replace("Job function", "")
+            industry = job_criteria[3].text.replace("Industries", "")
+
+            job_data = JobData(
+                name=row["name"],
+                seniority=seniority,
+                position_type=position_type,
+                job_function=job_function,
+                industry=industry
+            )
+            job_pipeline.add_data(job_data)
+            job_pipeline.close_pipeline()
+            success = True
+
+        except Exception as e:
+            logger.error(f"Exception thrown: {e}")
+            logger.warning(f"Failed to process page: {row['url']}, retries left: {retries-tries}")
+            tries += 1
+
+        finally:
+            driver.quit()
+            
+    if not success:
+        raise Exception(f"Max Retries exceeded: {retries}")
+    else:
+        logger.info(f"Successfully parsed: {row['url']}")
+
+
+
+
+def process_results(csv_file, location, retries=3):
+    logger.info(f"processing {csv_file}")
+    with open(csv_file, newline="") as file:
+        reader = list(csv.DictReader(file))
+
+        for row in reader:
+            process_posting(row, location, retries=retries)
+
+if __name__ == "__main__":
+
+    MAX_RETRIES = 3
+    MAX_THREADS = 5
+    PAGES = 1
+    LOCATION = "us"
+    LOCALITY = "United States"
+
+    logger.info(f"Crawl starting...")
+
+    ## INPUT ---> List of keywords to scrape
+    keyword_list = ["software engineer"]
+    aggregate_files = []
+
+    ## Job Processes
+    for keyword in keyword_list:
+        filename = keyword.replace(" ", "-")
+
+        crawl_pipeline = DataPipeline(csv_filename=f"{filename}.csv")
+        start_scrape(keyword, PAGES, LOCATION, LOCALITY, data_pipeline=crawl_pipeline, max_threads=MAX_THREADS, retries=MAX_RETRIES)
+        crawl_pipeline.close_pipeline()
+        aggregate_files.append(f"{filename}.csv")
+    logger.info(f"Crawl complete.")
+
+    for file in aggregate_files:
+        process_results(file, LOCATION, retries=MAX_RETRIES)
